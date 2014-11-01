93bec3bff9c88b31acd7010e8d43c8489bb27adb
j8lp <jelane@alumni.cmu.edu>
2014-11-01 11:30:29 -0700

    tesr

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-a
-makeCacheMatrix <- function(x = matrix()) {
+## Returns a special 'matrix' with get, set, getinverse, and setinverse functions. 
+##Also, when you initalize this special matrix, it will automatically set to the input 
 
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  set(x)
+  
+  list(set=set, get=get,
+       setinverse=setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a special matrix x.  X is assumed to be inversible and square.
+##If x already has cached the inverse, then just returns the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(s)
+  s}
+
